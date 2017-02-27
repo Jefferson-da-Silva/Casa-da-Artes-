3d37ba78d70965ff25463b4b1f58d65318a51abb
Jefferson <jeffersonsp58@gmail.com>
2017-02-27 17:18:32 -0300

    projeto

---
+++ file.js
@@ -0,0 +1,9 @@
+var newtable= document.createElement("table");
+var head= document.createElement("thread");
+var body= document.createElement("tbody");
+newtable.appendChild(head);
+newtable.appendChild(body);
+document.getElementById("divi").appendChild(newtable);
+function produtos(){
+	 document.getElementById("divi").innerHTML = "<img src='images/Projeto5.jpeg'>" + "<img src='images/Projeto6.jpeg'>";	
+}
